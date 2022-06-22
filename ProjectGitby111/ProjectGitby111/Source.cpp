64c262016fb4a9849da350a30fe341b0e4a24eaf
DmytroKhoptian <48955602+DmytroKhoptian@users.noreply.github.com>
2022-06-22 12:53:32 +0300

    Add Typedef  ull

--- ProjectGitby111/ProjectGitby111/Source.cpp
+++ ProjectGitby111/ProjectGitby111/Source.cpp
@@ -3,8 +3,29 @@
 
 using namespace std;
 
+using d_t = double;//ѕсевдонимы типов способ 1
+typedef unsigned long long  ull_t;  //ѕсевдонимы типов способ 2
+
+
+unsigned long long i1 = 99l;
+ull_t i2 = 99l;
+d_t i3 = 99.9;
+
+ull_t add(ull_t a, ull_t b)
+{
+	return a + b;
+}
+
+
+
 void main()
 {
+	for (size_t i = 0; i < 5; i++)
+	{
+
+	}
+
+
 	cout<<"Hello";
 	setlocale(LC_ALL, "");
 	system("pause");
